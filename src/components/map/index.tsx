a3b587665f649d347552325d108e2c8312aeb186
lucasoeth <lucasoeth@gmail.com>
2021-03-16 22:19:48 +0100

    made viewState a state in root component

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
-import MapGL from './mapgl';
-// interface Props {}
+import React, { FC, useState } from 'react';
+// import MapGL from './mapGL';
+import MapLayer from './mapLayer';
+import MapControls from './mapControls';
 
-const Map = () => {
-  return <MapGL />;
+import { ViewState } from './types';
+
+const initialViewState: ViewState = {
+  bearing: 0,
+  latitude: 52.500869,
+  longitude: 13.419047,
+  maxZoom: 19,
+  minZoom: 9,
+  pitch: 45,
+  zoom: 11,
+};
+
+const Map: FC = () => {
+  const [viewState, setViewState] = useState(initialViewState);
+
+  const onViewStateChange = (e: { viewState: ViewState }) =>
+    setViewState({ ...viewState, ...e.viewState });
+
+  return (
+    <MapLayer viewState={viewState} onViewStateChange={onViewStateChange} />
+  );
 };
 
 export default Map;
