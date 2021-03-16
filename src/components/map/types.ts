7f72171112bc695e76f44ab616b0b8119ea85f39
lucasoeth <lucasoeth@gmail.com>
2021-03-16 22:15:31 +0100

    added types

---
+++ src/components/map/types.ts
@@ -0,0 +1,29 @@
+export interface ViewState {
+  bearing: number;
+  latitude: number;
+  longitude: number;
+  maxZoom: number;
+  minZoom: number;
+  pitch: number;
+  zoom: number;
+}
+
+export interface FeatureCollectionSignature {
+  type: string;
+  features: FeatureSignature[];
+}
+
+export interface FeatureSignature {
+  type: string;
+  geometry: {
+    type: string;
+    coordinates: number[];
+  };
+  properties: {
+    id: string;
+    radolan_sum: number;
+    age: number;
+  };
+}
+
+export type OnViewStateChange = (e: unknown) => void;
