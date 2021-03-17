96612be59345127ba57df5dc393b2a835c17d90c
lucasoeth <lucasoeth@gmail.com>
2021-03-17 21:14:52 +0100

    made more detailed types

--- src/components/map/types.ts
+++ src/components/map/types.ts
@@ -1,3 +1,7 @@
+export interface Generic {
+  [key: string]: any;
+}
+
 export interface ViewState {
   bearing: number;
   latitude: number;
@@ -8,23 +12,64 @@ export interface ViewState {
   zoom: number;
 }
 
-export interface FeatureCollectionSignature {
-  type: string;
-  features: FeatureSignature[];
-}
-
-export interface FeatureSignature {
+export interface Feature {
   type: string;
   geometry: {
     type: string;
-    coordinates: number[];
+    coordinates: number[] | number[][];
   };
   properties: {
+    id: string | number;
+  };
+}
+
+export interface TreeFeature extends Feature {
+  properties: {
     id: string;
-    radolan_sum: number;
+    radolan_sum?: number;
     age: number;
   };
 }
 
+export interface PumpFeature extends Feature {
+  properties: {
+    id: number;
+    'addr:full': string;
+    check_date: string;
+    image: string;
+    'pump:status': string;
+    'pump:style': string;
+  };
+}
+
+export interface RainFeature extends Feature {
+  properties: {
+    id: number;
+    data: number[];
+  };
+}
+
+export interface FeatureCollection {
+  type: string;
+  features: TreeFeature[] | PumpFeature[] | RainFeature[];
+}
+
 export type OnViewStateChange = (e: unknown) => void;
 export type SetSelectedTree = (id: string) => void;
+export type SetLayer = (layer: string) => void;
+
+export interface HoveredPump {
+  message: string;
+  pointer: number[];
+}
+
+export type SetHoveredPump = (info: HoveredPump) => void;
+
+//Color.ts
+export type RGBAColor = [number, number, number, number];
+
+export interface Color extends Generic {
+  name: string;
+  rgba: RGBAColor;
+  hex: string;
+}
