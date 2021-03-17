1e2ded37805c93ca43723d9ada315305be621d12
lucasoeth <lucasoeth@gmail.com>
2021-03-17 11:48:06 +0100

    pass down states

--- src/components/map/mapLayer.tsx
+++ src/components/map/mapLayer.tsx
@@ -5,44 +5,42 @@ import { _MapContext as MapContext, StaticMap } from 'react-map-gl';
 import { ContextProviderValue } from '@deck.gl/core/lib/deck';
 
 import MapControls from './mapControls';
-import { OnViewStateChange, ViewState } from './types';
+import { ViewState, OnViewStateChange, SetSelectedTree } from './types';
 
-import trees from '../../assets/small_trees.json';
+// import trees from '../../assets/small_trees.json';
+import trees from '../../assets/trees_medium.json';
 import { getLayers } from './layer';
-import { GeoJsonLayer, GeoJsonLayerProps } from '@deck.gl/layers';
 
 interface Props {
+  isMobile: boolean;
   viewState: ViewState;
   onViewStateChange: OnViewStateChange;
+  selectedTree: string;
+  setSelectedTree: SetSelectedTree;
 }
 
-const MapLayer = ({ viewState, onViewStateChange }: Props): JSX.Element => {
-  // const layers = getLayers(trees);
-
-  const layers = [
-    new GeoJsonLayer({
-      id: 'geojson-layer',
-      data: trees.features,
-      pickable: true,
-      stroked: false,
-      filled: true,
-      extruded: true,
-      lineWidthScale: 20,
-      lineWidthMinPixels: 2,
-      getFillColor: [255, 0, 0, 200],
-      getLineColor: [0, 255, 0, 200],
-      getRadius: 10,
-      getLineWidth: 1,
-      getElevation: 30,
-    }),
-  ];
+const MapLayer = ({
+  isMobile,
+  viewState,
+  onViewStateChange,
+  selectedTree,
+  setSelectedTree,
+}: Props): JSX.Element => {
+  const layers = getLayers(
+    isMobile,
+    onViewStateChange,
+    trees,
+    true,
+    selectedTree,
+    setSelectedTree
+  );
 
   return (
     <DeckGL
       viewState={viewState}
       onViewStateChange={onViewStateChange}
       controller={true}
-      layers={getLayers(onViewStateChange, trees)}
+      layers={layers}
       ContextProvider={
         (MapContext.Provider as unknown) as React.Provider<ContextProviderValue>
       }
