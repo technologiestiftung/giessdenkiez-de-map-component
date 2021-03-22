e79bf5820ac5c3bdfae21567b51d226b4d17ab13
lucasoeth <lucasoeth@gmail.com>
2021-03-22 13:37:12 +0100

    added legend

---
+++ src/components/map/legend.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { SetLayer } from './types';
+
+const LegendContainer = styled.div`
+  z-index: 2;
+  position: absolute;
+  bottom: 12px;
+  right: 12px;
+  background: white;
+  :hover {
+    cursor: pointer;
+  }
+  p {
+    font-size: 12px;
+    margin: 0;
+    padding: 6px 12px;
+  }
+  p:hover {
+    background: #aaa;
+  }
+`;
+
+interface LegendProps {
+  setLayer: SetLayer;
+}
+
+const Legend = ({ setLayer }: LegendProps): JSX.Element => {
+  return (
+    <LegendContainer>
+      {['trees', 'pumps', 'rain'].map((layer, key) => {
+        return (
+          <p key={key} onClick={() => setLayer(layer)}>
+            {layer}
+          </p>
+        );
+      })}
+    </LegendContainer>
+  );
+};
+
+export default Legend;
