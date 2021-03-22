09a0aeef13b955e6882d07f1f6d353b2c71b6435
ff6347 <fabianmoronzirfas@protonmail.ch>
2021-03-22 17:51:30 +0100

    test(snapshot): Added inline snapshot example

--- src/components/map/layer.test.ts
+++ src/components/map/layer.test.ts
@@ -59,26 +59,219 @@ const test_rain = {
   ],
 };
 
-const test_res = "[{\"props\":{\"id\":\"tree\",\"pickable\":true,\"stroked\":true,\"filled\":true,\"extruded\":true,\"visible\":true,\"opacity\":1,\"getRadius\":3,\"pointRadiusMinPixels\":0.5,\"getLineColor\":[247,105,6,255],\"autoHighlight\":true,\"highlightColor\":[200,200,200,255],\"updateTriggers\":{\"getLineWidth\":[\"\"]}},\"id\":\"tree\",\"count\":0,\"lifecycle\":\"Awaiting state\",\"parent\":null,\"context\":null,\"state\":null,\"internalState\":null},{\"props\":{\"id\":\"pumps\",\"pickable\":true,\"stroked\":true,\"filled\":true,\"extruded\":true,\"visible\":false,\"opacity\":1,\"getRadius\":9,\"pointRadiusMinPixels\":4,\"getLineColor\":[0,0,0,200],\"getLineWidth\":3,\"lineWidthMinPixels\":1.5},\"id\":\"pumps\",\"count\":1,\"lifecycle\":\"Awaiting state\",\"parent\":null,\"context\":null,\"state\":null,\"internalState\":null},{\"props\":{\"id\":\"rain\",\"pickable\":true,\"stroked\":false,\"filled\":true,\"extruded\":true,\"wireframe\":true,\"visible\":false,\"opacity\":0.95,\"getElevation\":1,\"getRadius\":9,\"pointRadiusMinPixels\":4,\"getLineColor\":[0,0,0,200],\"getLineWidth\":3,\"lineWidthMinPixels\":1.5},\"id\":\"rain\",\"count\":2,\"lifecycle\":\"Awaiting state\",\"parent\":null,\"context\":null,\"state\":null,\"internalState\":null}]"
-
 describe('DeckGL Layer Genration Function', () => {
   test('get all layers not mobile', () => {
     expect(
-      JSON.stringify(
-        getLayers(
-          false,
-          () => {},
-          true,
-          false,
-          false,
-          test_trees,
-          test_pumps,
-          test_rain,
-          '',
-          () => {},
-          () => {}
-        )
+      getLayers(
+        false,
+        () => {},
+        true,
+        false,
+        false,
+        test_trees,
+        test_pumps,
+        test_rain,
+        '',
+        () => {},
+        () => {}
       )
-    ).toBe(test_res);
+    ).toMatchInlineSnapshot(`
+      Array [
+        GeoJsonLayer {
+          "context": null,
+          "count": 0,
+          "id": "tree",
+          "internalState": null,
+          "lifecycle": "Awaiting state",
+          "parent": null,
+          "props": Object {
+            "autoHighlight": true,
+            "extruded": true,
+            "filled": true,
+            "getFillColor": [Function],
+            "getLineColor": Array [
+              247,
+              105,
+              6,
+              255,
+            ],
+            "getLineWidth": [Function],
+            "getRadius": 3,
+            "highlightColor": Array [
+              200,
+              200,
+              200,
+              255,
+            ],
+            "id": "tree",
+            "onClick": [Function],
+            "opacity": 1,
+            "pickable": true,
+            "pointRadiusMinPixels": 0.5,
+            "stroked": true,
+            "updateTriggers": Object {
+              "getLineWidth": Array [
+                "",
+              ],
+            },
+            "visible": true,
+            Symbol(component): [Circular],
+            Symbol(asyncPropOriginal): Object {},
+            Symbol(asyncPropResolved): Object {
+              "data": Array [
+                Object {
+                  "geometry": Object {
+                    "coordinates": Array [
+                      13.50213,
+                      52.645680000000006,
+                    ],
+                    "type": "Point",
+                  },
+                  "properties": Object {
+                    "age": 2,
+                    "id": "nt240620302094",
+                    "radolan_sum": 43.9,
+                  },
+                  "type": "Feature",
+                },
+              ],
+            },
+          },
+          "state": null,
+        },
+        GeoJsonLayer {
+          "context": null,
+          "count": 1,
+          "id": "pumps",
+          "internalState": null,
+          "lifecycle": "Awaiting state",
+          "parent": null,
+          "props": Object {
+            "extruded": true,
+            "filled": true,
+            "getFillColor": [Function],
+            "getLineColor": Array [
+              0,
+              0,
+              0,
+              200,
+            ],
+            "getLineWidth": 3,
+            "getRadius": 9,
+            "id": "pumps",
+            "lineWidthMinPixels": 1.5,
+            "onHover": [Function],
+            "opacity": 1,
+            "pickable": true,
+            "pointRadiusMinPixels": 4,
+            "stroked": true,
+            "visible": false,
+            Symbol(component): [Circular],
+            Symbol(asyncPropOriginal): Object {},
+            Symbol(asyncPropResolved): Object {
+              "data": Array [
+                Object {
+                  "geometry": Object {
+                    "coordinates": Array [
+                      13.564219,
+                      52.5398067,
+                    ],
+                    "type": "Point",
+                  },
+                  "id": "0",
+                  "properties": Object {
+                    "addr:full": "Schragenfeldstraße 25",
+                    "check_date": "unbekannt",
+                    "id": 352734260,
+                    "image": "File:Plumpe 11 Marzahn Schragenfeldstraße-Bäckerpfuhl (8).jpg",
+                    "pump:status": "unbekannt",
+                    "pump:style": "Borsig",
+                  },
+                  "type": "Feature",
+                },
+              ],
+            },
+          },
+          "state": null,
+        },
+        GeoJsonLayer {
+          "context": null,
+          "count": 2,
+          "id": "rain",
+          "internalState": null,
+          "lifecycle": "Awaiting state",
+          "parent": null,
+          "props": Object {
+            "extruded": true,
+            "filled": true,
+            "getElevation": 1,
+            "getFillColor": [Function],
+            "getLineColor": Array [
+              0,
+              0,
+              0,
+              200,
+            ],
+            "getLineWidth": 3,
+            "getRadius": 9,
+            "id": "rain",
+            "lineWidthMinPixels": 1.5,
+            "opacity": 0.95,
+            "pickable": true,
+            "pointRadiusMinPixels": 4,
+            "stroked": false,
+            "visible": false,
+            "wireframe": true,
+            Symbol(component): [Circular],
+            Symbol(asyncPropOriginal): Object {},
+            Symbol(asyncPropResolved): Object {
+              "data": Array [
+                Object {
+                  "geometry": Object {
+                    "coordinates": Array [
+                      Array [
+                        Array [
+                          13.4971096103036,
+                          52.6543762343589,
+                        ],
+                        Array [
+                          13.5042366641048,
+                          52.6541120206218,
+                        ],
+                        Array [
+                          13.5033652084477,
+                          52.64547732806,
+                        ],
+                        Array [
+                          13.4891325166817,
+                          52.6460049371748,
+                        ],
+                        Array [
+                          13.4900002353473,
+                          52.654638719058,
+                        ],
+                        Array [
+                          13.4971096103036,
+                          52.6543762343589,
+                        ],
+                      ],
+                    ],
+                    "type": "Polygon",
+                  },
+                  "properties": Object {
+                    "data": Array [
+                      571,
+                    ],
+                    "id": 1,
+                  },
+                  "type": "Feature",
+                },
+              ],
+            },
+          },
+          "state": null,
+        },
+      ]
+    `);
   });
 });
