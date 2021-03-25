3a95b1bb482ed977d35a0219180b3d6c0fe049f2
lucasoeth <lucasoeth@gmail.com>
2021-03-25 12:44:07 +0100

    test(layer): added tests for getLayers

--- src/components/layer/layer.test.ts
+++ src/components/layer/layer.test.ts
@@ -1,6 +1,8 @@
-import { getLayers } from './layer';
+import { getLayers, getPumpLayer, getRainLayer } from '.';
 
-const test_trees = {
+import { FeatureCollection } from 'src/types';
+
+const test_trees: FeatureCollection = {
   type: 'FeatureCollection',
   features: [
     {
@@ -11,7 +13,7 @@ const test_trees = {
   ],
 };
 
-const test_pumps = {
+const test_pumps: FeatureCollection = {
   type: 'FeatureCollection',
   features: [
     {
@@ -33,7 +35,7 @@ const test_pumps = {
   ],
 };
 
-const test_rain = {
+const test_rain: FeatureCollection = {
   type: 'FeatureCollection',
   features: [
     {
@@ -59,219 +61,31 @@ const test_rain = {
   ],
 };
 
-describe('DeckGL Layer Genration Function', () => {
-  test('get all layers not mobile', () => {
-    expect(
-      getLayers(
-        false,
-        () => {},
-        true,
-        false,
-        false,
-        test_trees,
-        test_pumps,
-        test_rain,
-        '',
-        () => {},
-        () => {}
-      )
-    ).toMatchInlineSnapshot(`
-      Array [
-        GeoJsonLayer {
-          "context": null,
-          "count": 0,
-          "id": "tree",
-          "internalState": null,
-          "lifecycle": "Awaiting state",
-          "parent": null,
-          "props": Object {
-            "autoHighlight": true,
-            "extruded": true,
-            "filled": true,
-            "getFillColor": [Function],
-            "getLineColor": Array [
-              247,
-              105,
-              6,
-              255,
-            ],
-            "getLineWidth": [Function],
-            "getRadius": 3,
-            "highlightColor": Array [
-              200,
-              200,
-              200,
-              255,
-            ],
-            "id": "tree",
-            "onClick": [Function],
-            "opacity": 1,
-            "pickable": true,
-            "pointRadiusMinPixels": 0.5,
-            "stroked": true,
-            "updateTriggers": Object {
-              "getLineWidth": Array [
-                "",
-              ],
-            },
-            "visible": true,
-            Symbol(component): [Circular],
-            Symbol(asyncPropOriginal): Object {},
-            Symbol(asyncPropResolved): Object {
-              "data": Array [
-                Object {
-                  "geometry": Object {
-                    "coordinates": Array [
-                      13.50213,
-                      52.645680000000006,
-                    ],
-                    "type": "Point",
-                  },
-                  "properties": Object {
-                    "age": 2,
-                    "id": "nt240620302094",
-                    "radolan_sum": 43.9,
-                  },
-                  "type": "Feature",
-                },
-              ],
-            },
-          },
-          "state": null,
-        },
-        GeoJsonLayer {
-          "context": null,
-          "count": 1,
-          "id": "pumps",
-          "internalState": null,
-          "lifecycle": "Awaiting state",
-          "parent": null,
-          "props": Object {
-            "extruded": true,
-            "filled": true,
-            "getFillColor": [Function],
-            "getLineColor": Array [
-              0,
-              0,
-              0,
-              200,
-            ],
-            "getLineWidth": 3,
-            "getRadius": 9,
-            "id": "pumps",
-            "lineWidthMinPixels": 1.5,
-            "onHover": [Function],
-            "opacity": 1,
-            "pickable": true,
-            "pointRadiusMinPixels": 4,
-            "stroked": true,
-            "visible": false,
-            Symbol(component): [Circular],
-            Symbol(asyncPropOriginal): Object {},
-            Symbol(asyncPropResolved): Object {
-              "data": Array [
-                Object {
-                  "geometry": Object {
-                    "coordinates": Array [
-                      13.564219,
-                      52.5398067,
-                    ],
-                    "type": "Point",
-                  },
-                  "id": "0",
-                  "properties": Object {
-                    "addr:full": "Schragenfeldstraße 25",
-                    "check_date": "unbekannt",
-                    "id": 352734260,
-                    "image": "File:Plumpe 11 Marzahn Schragenfeldstraße-Bäckerpfuhl (8).jpg",
-                    "pump:status": "unbekannt",
-                    "pump:style": "Borsig",
-                  },
-                  "type": "Feature",
-                },
-              ],
-            },
-          },
-          "state": null,
-        },
-        GeoJsonLayer {
-          "context": null,
-          "count": 2,
-          "id": "rain",
-          "internalState": null,
-          "lifecycle": "Awaiting state",
-          "parent": null,
-          "props": Object {
-            "extruded": true,
-            "filled": true,
-            "getElevation": 1,
-            "getFillColor": [Function],
-            "getLineColor": Array [
-              0,
-              0,
-              0,
-              200,
-            ],
-            "getLineWidth": 3,
-            "getRadius": 9,
-            "id": "rain",
-            "lineWidthMinPixels": 1.5,
-            "opacity": 0.95,
-            "pickable": true,
-            "pointRadiusMinPixels": 4,
-            "stroked": false,
-            "visible": false,
-            "wireframe": true,
-            Symbol(component): [Circular],
-            Symbol(asyncPropOriginal): Object {},
-            Symbol(asyncPropResolved): Object {
-              "data": Array [
-                Object {
-                  "geometry": Object {
-                    "coordinates": Array [
-                      Array [
-                        Array [
-                          13.4971096103036,
-                          52.6543762343589,
-                        ],
-                        Array [
-                          13.5042366641048,
-                          52.6541120206218,
-                        ],
-                        Array [
-                          13.5033652084477,
-                          52.64547732806,
-                        ],
-                        Array [
-                          13.4891325166817,
-                          52.6460049371748,
-                        ],
-                        Array [
-                          13.4900002353473,
-                          52.654638719058,
-                        ],
-                        Array [
-                          13.4971096103036,
-                          52.6543762343589,
-                        ],
-                      ],
-                    ],
-                    "type": "Polygon",
-                  },
-                  "properties": Object {
-                    "data": Array [
-                      571,
-                    ],
-                    "id": 1,
-                  },
-                  "type": "Feature",
-                },
-              ],
-            },
-          },
-          "state": null,
-        },
-      ]
-    `);
+const test_communityData = {
+  _00l734zv4: {
+    watered: true,
+    adopted: false,
+  },
+};
+
+const emptyFunction = function () {
+  return 0;
+};
+
+describe('DeckGL Layer Generation Functions', () => {
+  // test('getTreeLayer(showPumps=true, pumps=test_pumps, setHoveredPump=emptyFunction)', () => {
+  //   expect(getTreeLayer(true, test_pumps, emptyFunction)).toMatchSnapshot();
+  // });
+  test('getPumpLayer(showPumps=true, pumps=test_pumps, setHoveredPump=emptyFunction)', () => {
+    expect(getPumpLayer(true, test_pumps, emptyFunction)).toMatchSnapshot();
+  });
+  test('getPumpLayer(showPumps=false, pumps=test_pumps, setHoveredPump=emptyFunction)', () => {
+    expect(getPumpLayer(false, test_pumps, emptyFunction)).toMatchSnapshot();
+  });
+  test('getRainLayer(showRain=true, rain=test_rain)', () => {
+    expect(getRainLayer(true, test_rain)).toMatchSnapshot();
+  });
+  test('getRainLayer(showRain=false, rain=test_rain)', () => {
+    expect(getRainLayer(false, test_rain)).toMatchSnapshot();
   });
 });
